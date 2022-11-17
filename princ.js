c22725f721b44107a44b23b8fcf9d45af528ddf8
maximelouette <maxime_louette@hotmail.com>
2022-11-17 21:07:47 +0100

    Add files via upload

--- princ.js
+++ princ.js
@@ -3,7 +3,7 @@ var translate = 0;
 
 window.addEventListener('click', function(e) {
 	random = Math.floor(Math.random()*8);
-	random = random + 100;
+	random = random + 80;
 	translate = translate + 150 * random;
 	heroes.style.backgroundPosition = translate+'px 0px';
 })
